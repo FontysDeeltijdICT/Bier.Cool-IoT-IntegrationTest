e8d33f4454f247581d8d6156d5bbc2ef04c8db3d
Mitchell Ton <mitchell@vention.nl>
2021-06-09 00:45:32 +0200

    added hardware stub

---
+++ stubs/HardwareStub.js
@@ -0,0 +1,31 @@
+var mqtt = require('mqtt');
+
+var HardwareDeviceStub = function () {};
+
+HardwareDeviceStub.prototype.connect = function (endpoint) {
+	HardwareDeviceStub.prototype.client  = mqtt.connect(endpoint);
+};
+
+HardwareDeviceStub.prototype.send = function (deviceid, temp1, temp2, pres1, pres2) {
+	HardwareDeviceStub.prototype.client.publish(
+		'beers/v1/' + deviceid + '/data', 
+		`{
+			"sensor1": 
+			{
+				"temperature": `+temp1+`,
+				"presence": `+pres1+`
+			},
+			"sensor2":
+			{
+				"temperature": `+temp2+`,
+				"presence": `+pres2+`
+			}
+		}`
+	);
+};
+
+HardwareDeviceStub.prototype.disconnect = function (endpoint) {
+	HardwareDeviceStub.prototype.client.disconnect();
+};
+
+module.exports = new HardwareDeviceStub();
